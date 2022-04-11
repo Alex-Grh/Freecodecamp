07f619abd74493ab6ca08463de6cd9e3b9d66747
Alex-Grh <alex_grh@mail.ru>
2022-04-11 16:40:36 +0300

    1

--- JS/56 Assignment with a Returned Value.js
+++ JS/56 Assignment with a Returned Value.js
@@ -13,3 +13,13 @@ processed should have a value of 2
 
 You should assign processArg to processed
 */
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+  
+}
+processed= processArg(7);
+processArg();
+// Only change code below this line

131d81bc80f6970bb8f5e5ffcd82473c31ae87fe
Alex-Grh <alex_grh@mail.ru>
2022-04-12 11:38:55 +0300

    1

--- JS/69 Comparisons with the Logical And Operator.js
+++ JS/69 Comparisons with the Logical And Operator.js
@@ -38,3 +38,17 @@ testLogicalAnd(75) should return the string No
 
 testLogicalAnd(80) should return the string No
 */
+function testLogicalAnd(val) {
+   // Only change code below this line
+ 
+   if (val >= 25 && val <= 50) {
+     
+       return "Yes";
+     
+   }
+ 
+   // Only change code above this line
+   return "No";
+ }
+ 
+ testLogicalAnd(10);

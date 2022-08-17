ed8afdf1777ce53db92c7f3b572b847a3ec8adc4
Alex-Grh <alex_grh@mail.ru>
2022-08-17 22:52:45 +0400

    1

--- JS/194 Add Items Using splice.js
+++ JS/194 Add Items Using splice.js
@@ -20,3 +20,11 @@ Waiting:The htmlColorNames function should utilize the splice() method
 Waiting:You should not use shift() or unshift().
 Waiting:You should not use array bracket notation.
 */
+function htmlColorNames(arr) {
+   // Only change code below this line
+   arr.splice(0, 2, 'DarkSalmon', 'BlanchedAlmond');
+   // Only change code above this line
+   return arr;
+}
+
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));

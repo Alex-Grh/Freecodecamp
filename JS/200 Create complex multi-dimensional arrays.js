985048f6126a525b1272c542fcd1892142402397
Alex-Grh <alex_grh@mail.ru>
2022-08-28 20:33:21 +0400

    1

--- JS/200 Create complex multi-dimensional arrays.js
+++ JS/200 Create complex multi-dimensional arrays.js
@@ -41,3 +41,13 @@ Waiting:myNestedArray should contain exactly one occurrence of the string deep o
 Waiting:myNestedArray should contain exactly one occurrence of the string deeper on an array nested 4 levels deep
 Waiting:myNestedArray should contain exactly one occurrence of the string deepest on an array nested 5 levels deep
 */
+
+let myNestedArray = [
+   // change code below this line
+   ["unshift", false, 1, 2, 3, "complex", "nested"],
+   ["loop", "shift", 6, 7, 1000, "method"],
+   ["concat", false, true, "spread", "array", ["deep"]],
+   ["mutate", 1327.98, "splice", "slice", "push", [["deeper"]]],
+   ["iterate", 1.3849, 7, "8.4876", "arbitrary", "depth", [[["deepest"]]]]
+   // change code above this line
+];

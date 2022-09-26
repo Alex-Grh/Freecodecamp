dfb0651375376b1829d2c1645aad12dd884af455
Alex-Grh <alex_grh@mail.ru>
2022-09-26 23:58:17 +0400

    1

---
+++ JS/210 Reverse a String.js
@@ -0,0 +1,19 @@
+/*
+Reverse a String
+Reverse the provided string.
+
+You may need to turn the string into an array before you can reverse it.
+
+Your result must be a string.
+
+Tests
+Waiting:reverseString("hello") should return a string.
+Waiting:reverseString("hello") should return the string olleh.
+Waiting:reverseString("Howdy") should return the string ydwoH.
+Waiting:reverseString("Greetings from Earth") should return the string htraE morf sgniteerG.
+*/
+function reverseString(str) {
+   return str.reverse();
+ }
+ 
+ reverseString("hello");

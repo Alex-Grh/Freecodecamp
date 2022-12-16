888259a0750b0d77e775ee2d8d1e3c4ad75ad4b4
Alex-Grh <alex_grh@mail.ru>
2022-12-16 23:46:29 +0400

    1

--- JS/233 Understand Own Properties.js
+++ JS/233 Understand Own Properties.js
@@ -29,3 +29,17 @@ Waiting:ownProps should include the values numLegs and name.
 Waiting:You should solve this challenge without using the built in method Object.keys().
 Waiting:You should solve this challenge without hardcoding the ownProps array.
 */
+
+function Bird(name) {
+   this.name = name;
+   this.numLegs = 2;
+ }
+ 
+ let canary = new Bird("Tweety");
+ let ownProps = [];
+ // Only change code below this line
+ for (let property in canary) {
+   if(canary.hasOwnProperty(property)) {
+     ownProps.push(property);
+   }
+ }

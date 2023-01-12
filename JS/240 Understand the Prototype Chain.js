2e5bcb8b8b2e25a497019e0a37b8aab823bcfff0
Alex-Grh <alex_grh@mail.ru>
2023-01-12 23:44:55 +0400

    1

--- JS/240 Understand the Prototype Chain.js
+++ JS/240 Understand the Prototype Chain.js
@@ -33,3 +33,14 @@ Dog.prototype.isPrototypeOf(beagle);  // yields true
 // Fix the code below so that it evaluates to true
 ???.isPrototypeOf(Dog.prototype);
 */
+
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);  // yields true
+
+// Fix the code below so that it evaluates to true
+Object.prototype.isPrototypeOf(Dog.prototype);

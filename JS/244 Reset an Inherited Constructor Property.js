55a41f5948538dac78f148bdc6df6e7d3fe11024
Alex-Grh <alex_grh@mail.ru>
2023-01-18 21:23:44 +0400

    1

--- JS/244 Reset an Inherited Constructor Property.js
+++ JS/244 Reset an Inherited Constructor Property.js
@@ -35,3 +35,20 @@ Dog.prototype = Object.create(Animal.prototype);
 let duck = new Bird();
 let beagle = new Dog();
 */
+
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+// Only change code below this line
+
+let duck = new Bird();
+let beagle = new Dog();
+
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+duck.constructor;
+beagle.constructor;
